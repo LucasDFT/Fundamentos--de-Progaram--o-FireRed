580a5170911362dbafbb44c5c36949debc31c4ea
Lucas F. <fonsecalucas642@gmai.com>
2026-03-24 09:50:02 -0300

    subindo arquivos

---
+++ Aula-06/Estrutura_Pizzaria.py
@@ -0,0 +1,22 @@
+# Variáveis da Pizzaria  
+nome = (input("Por Favor, informe seu nome a seguir para que possamos prosseguir: "))
+
+pizza_sabor= (input("Informe por favor, o sabor de pizza que você deseja solicitar — [Frango com Requeijão], [Calabresa], [Mussarela] e [Banana com Chocolate]: "))
+
+pizza_tamanho= (input("Por favor, informe o tamanho que você deseja — [Pequena], [Média] e [Grande]: "))
+
+dia_semana= (input("Informe por favor, o dia da semana em que estamos — [Quarta], [Quinta], [Sexta], [Sábado] e [Domingo] "))
+
+
+
+print (f"Olá {nome} o sabor de pizza escolhido por você foi {pizza_sabor}, já o tamanho desejado é {pizza_tamanho} e o dia de hoje é {dia_semana}")
+# Promoções -> Estruturas Condicionais
+
+# O Refri é gratuito caso a compra seja realizado aos sábados.
+
+# Comprando uma pizza de calabresa tamanho médio, em qualquer dia, o frete é gratuito.
+
+# Comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos.
+
+
+
--- Aula-06/README.md
+++ Aula-06/README.md
@@ -12,8 +12,15 @@
 * -> Multiplicação
 / -> Divisão 
 
+## Operadores Lógicos
+1. And-> e -> Se duas condiçoes forem verdadeiras, o resultado é verdadeiro.
+2. Or -> our -> Se pelo menos uma de duas ou mais condições forem verdadeiras, o resultado é verdadeiro.
+3. Not ->
+
+
 ## Métodos em Python
 1. Print() -> Exibe Informações no terminal.
+2. Input() -> Capturar uma informação no terminal.
 
 ## Format em Python
 1. print(f"bla bla bla {Váriavel}")
@@ -22,3 +29,4 @@
  ``If (se)`` -> Verifica se uma condição é True (verdadeira). Se for, ele executa o código.
  ``Elif (senão se)`` -> É usado para testar várias condições. Ele só executa se todas as condições anterioeres forem falsas.
 `` Else (senão)`` -> Executa o código de se a condição if for false (falsa).
+
